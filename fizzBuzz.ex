1d7a5ae69ab551d4dcc4054b318ec3fafc0095e4
Manuel Alonso <manuteali@gmail.com>
2013-05-24 20:55:16 +0200

    Update fizzBuzz.ex
    Align , show results

--- fizzBuzz.ex
+++ fizzBuzz.ex
@@ -1,17 +1,17 @@
 fizzBuzz = fn(0,0,_) -> IO.puts "FizzBuzz" ;
-			 (0,_,_) -> IO.puts "Fizz" ;			
-		     (_,0,_) -> IO.puts "Buzz" ;		 
-		     (_,_,n) -> IO.puts n
+	     (0,_,_) -> IO.puts "Fizz" ;			
+             (_,0,_) -> IO.puts "Buzz" ;		 
+	     (_,_,n) -> IO.puts n
 		 	
-		 	end
+	   end
 
 callFizzBuzz = fn(n) -> fizzBuzz.(rem(n,3),rem(n,5),n) end
 
 
-callFizzBuzz.(10)
-callFizzBuzz.(11)
-callFizzBuzz.(12)
-callFizzBuzz.(13)
-callFizzBuzz.(14)
-callFizzBuzz.(15)
-callFizzBuzz.(16)
+callFizzBuzz.(10) #Buzz 
+callFizzBuzz.(11) #11 
+callFizzBuzz.(12) #Fizz 
+callFizzBuzz.(13) #13 
+callFizzBuzz.(14) #14 
+callFizzBuzz.(15) #FizzBuzz 
+callFizzBuzz.(16) #16 
